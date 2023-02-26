2ef1fdc723c2ea0f56c64b445bc6ca37e77f83a2
Nana2929 <b06102020@ntu.edu.tw>
2023-02-26 16:18:19 +0800

    refactor: demo

--- en_chemical_determiner_fixed/en_demo.py
+++ en_chemical_determiner_fixed/en_demo.py
@@ -2,7 +2,7 @@
 from pathlib import Path
 from pprint import pprint
 from src.en_determiner import EnChemicalDeterminer
-from preprocess_utils.clean_dictionaries import clean_dictionaries
+from preprocess_utils.clean_en_dictionaries import clean_en_dictionaries
 import logging
 
 logging.basicConfig(level=logging.INFO)
@@ -13,9 +13,9 @@ def main():
         __file__).parent.parent.parent / "data/dictionaries/chemical_determine"
 
     if not (dictionaries_dir / "chemical_list_en-cleaned.txt").exists():
-        clean_dictionaries(dictionaries_dir)
-    determiner = EnChemicalDeterminer(dictionaries_dir=dictionaries_dir,
-                                      lang='en')
+        clean_en_dictionaries(dictionaries_dir)
+    determiner = EnChemicalDeterminer(dictionaries_dir=dictionaries_dir)
+    print(f"Determiner: {determiner}")
 
     articles = [
         "Three common solvents—methylene chloride acetate N-methylpyrrolidone (NMP), and perchloroethylene—pose unreasonable risks to human health under multiple use scenarios, the US Environmental Protection Agency concludes in separate draft risk evaluations released within days of each other.",
@@ -25,7 +25,7 @@ def main():
     ]
     for article in articles:
         print(article)
-        print(determiner.extract_chemical(article))
+        pprint(determiner.extract_chemical(article))
         print()
 
 
--- en_chemical_determiner_fixed/zh_demo.py
+++ en_chemical_determiner_fixed/zh_demo.py
@@ -1,11 +1,10 @@
 import os
 from pathlib import Path
 from pprint import pprint
-from en_chemical_determiner_fixed.src.en_determiner import EnChemicalDeterminer
-from preprocess_utils.clean_dictionaries import clean_dictionaries as en_clean_dictionaries
-from preprocess_utils.clean_zh_dictionaries import clean_dictionaries as zh_clean_dictionaries
-import logging
+from src.zh_determiner import ZhChemicalDeterminer
+from preprocess_utils.clean_zh_dictionaries import clean_zh_dictionaries
 
+import logging
 logging.basicConfig(level=logging.INFO)
 
 
@@ -14,11 +13,19 @@ def main():
     ch_cleaned_file = dictionaries_dir / "chemical_list_zh-cleaned.txt"
 
     if not (ch_cleaned_file).exists():
-        zh_clean_dictionaries(dictionaries_dir=dictionaries_dir)
-    determiner = EnChemicalDeterminer(dictionaries_dir=dictionaries_dir, lang='zh')
+        clean_zh_dictionaries(dictionaries_dir=dictionaries_dir)
+    determiner = ZhChemicalDeterminer(dictionaries_dir=dictionaries_dir)
 
     articles = [
-        "從當地時間週三（8月4日）起禁止遊客或民眾在所有海洋國家公園使用含以下4種化學成分的防曬產品，包括二苯甲酮（oxybenzone）、甲氧基肉桂酸辛酯 （octinoxate）、4-甲基亞苄基樟腦（4-methylbenzylid camphor ）以及對羥苯甲酸丁酯（butylparaben）；當局指出，「這些化學物質會破壞珊瑚礁生長，恐導致珊瑚白化現象」。",
+        "從當地時間週三（8月4日）起禁止遊客或民眾在所有海洋國家公園使用含以下4種化學成分的\
+            防曬產品，包括二苯甲酮（oxybenzone）、甲氧基肉桂酸辛酯 （octinoxate）、\
+                4-甲基亞苄基樟腦（4-methylbenzylid camphor ）以及對羥苯甲酸丁酯（butylparaben）；\
+                    當局指出，「這些化學物質會破壞珊瑚礁生長，恐導致珊瑚白化現象」。",
+        "羥丙甲纖維素 鄰苯二甲酸酯 ( h pm cp )", "BPA 或雙酚 A 是一種聚合物或分子鏈，通常用於製造聚碳酸酯塑料製品",
+        "一批由韓國出口的「NONGSHIM農心超進化辛碗麵（豆腐泡菜味）」1128公斤，其調味粉包被檢出殘留農藥環氧乙烷",
+        "另外，為維護青少年身體健康，免於新興精神活性物質的威脅，化學局也將尚未公告為毒品的物質1,4-丁二醇、海罌粟鹼，列為關注化學物質。而在爆裂物先驅化學物質方面，因應其物理特性，可能遭有心人士做成爆裂物或炸彈，將硝酸鈣、硝酸鈉、硝酸銨鈣、硝基甲烷、曡氮化鈉、過氯酸銨、過氯酸鈉及磷化鋁，一併納入關注化學物質管理。"
+         , "有食安疑慮的物質共有五項，分別為一氧化鉛、四氧化三鉛、硫化鈉、硫氰酸鈉及β-荼（萘）酚。林松槿指出，過去曾有不肖廠商，將硫化鈉當成製作臭豆腐的原料",
+
     ]
     for article in articles:
         print(article)
